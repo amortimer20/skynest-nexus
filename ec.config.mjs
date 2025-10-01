35c8bf44fc3cd6d481f7f8143c11a0116f4967cb
Anthony Mortimer <agm20@outlook.com>
2025-10-01 09:51:26 -0400

    Set default collapse style

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,23 +1,26 @@
 import { defineEcConfig } from "astro-expressive-code";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
-import { transformerColorizedBrackets } from '@shikijs/colorized-brackets';
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import { transformerColorizedBrackets } from "@shikijs/colorized-brackets";
 
 export default defineEcConfig({
-    themes: ['material-theme-ocean'],
-      frames: {
-        showCopyToClipboardButton: false
-      },
-      shiki: {
-        transformers: [transformerColorizedBrackets()]
-      },
-      plugins: [
-        pluginCollapsibleSections(),
-        pluginLineNumbers()
-      ],
-      styleOverrides: {
-        textMarkers: {
-          backgroundOpacity: '25%'
-        }
-      }
-})
+  themes: ["material-theme-ocean"],
+  frames: {
+    showCopyToClipboardButton: false,
+  },
+  shiki: {
+    transformers: [transformerColorizedBrackets()],
+  },
+  plugins: [
+    pluginCollapsibleSections(),
+    pluginLineNumbers(),
+  ],
+  styleOverrides: {
+    textMarkers: {
+      backgroundOpacity: "25%",
+    },
+  },
+  defaultProps: {
+    collapseStyle: "collapsible-auto",
+  },
+});
